efb7b55152ce550bc075455b5f8362fed4c9a9f9
Marc Sánchez <sanchezfauste@gmail.com>
2016-10-10 17:11:26 +0200

    Typo on usage help

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(int argc, const char *argv[]) {
     if (argc != 3) {
-        cout << "Use: " << argv[0] << "<nRows> <nCols>" << endl;
+        cout << "Use: " << argv[0] << " <nRows> <nCols>" << endl;
         return 1;
     }
     Map map = MapGenerator(atoi(argv[1]), atoi(argv[2])).getMap();
